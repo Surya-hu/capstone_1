e8622c024ddda3f45a382017b0077aa7655cf9e1
Surya <surya92kumaran@gmail.com>
2024-04-12 10:09:45 +0530

    new change

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define your Docker Compose file name
+COMPOSE_FILE="docker-compose.yml"
+
+# Define the name of the Docker image
+IMAGE_NAME="myreactapp"
+
+# Build the Docker image
+docker build -t $IMAGE_NAME .
+
+# Shut down all containers running using Docker Compose
+docker-compose -f $COMPOSE_FILE down
+
+# Bring up containers using Docker Compose
+docker-compose -f $COMPOSE_FILE up -d
+
---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define Docker Hub credentials
+DOCKER_USERNAME="surya92kumaran"
+DOCKER_PASSWORD="Mail@123@123"
+
+# Define Docker Hub repositories
+DEV_REGISTRY="surya92kumaran/dev_capstone"
+PROD_REGISTRY="surya92kumaran/prod_capstone"
+
+# Define the branch names
+DEV_BRANCH="dev"
+MAIN_BRANCH="main"
+
+# Function to authenticate with Docker Hub
+authenticate_docker_hub() {
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+}
+
+# Function to build and push Docker image to the specified registry
+build_and_push_image() {
+    local branch=$1
+    local registry=$2
+
+    echo "Building and pushing image to $registry"
+    docker build -t "$registry:$branch" .
+    docker tag "$registry:$branch" "$registry:latest"  # Tagging the image as "latest"
+    docker push "$registry:$branch"
+    docker push "$registry:latest"  # Pushing the "latest" tag
+}
+
+# Check the current branch and push images accordingly
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$current_branch" == "$DEV_BRANCH" ]; then
+    authenticate_docker_hub
+    build_and_push_image "$DEV_BRANCH" "$DEV_REGISTRY"
+elif [ "$current_branch" == "$MAIN_BRANCH" ]; then
+    authenticate_docker_hub
+    build_and_push_image "$MAIN_BRANCH" "$PROD_REGISTRY"
+else
+    echo "No action specified for branch $current_branch"
+fi
+
